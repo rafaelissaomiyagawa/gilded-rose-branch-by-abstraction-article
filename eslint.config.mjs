dc9058b0416fcf2e6a6be92badef04ec19ea9976
Rafael Issao Miyagawa <rafael.issao@gmail.com>
2024-10-31 15:03:18 -0300

    Updating some configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      eqeqeq: ["error", "smart"],
+    },
+  },
   pluginJs.configs.recommended,
 ];
